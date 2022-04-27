72d7de70593b3f8ca69fa2561542ddb207065ad0
Hemanth-D <hemanthh531@gmail.com>
2022-04-27 13:15:53 +0530

    part2 2.1: Remove components directory & modify App file to work

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,4 +1,37 @@
-import Course from "./components/Course"
+const Header = ({ courseName }) => {
+  return (
+    <h1>{courseName}</h1>
+  )
+}
+
+const Content = ({ courseParts }) => {
+  // console.log({ courseParts})
+  return (
+  // wrap the JSX expression in a fragment <> </> so that it has one parent element to the div 'root'
+  <div>
+      <Part partName={courseParts[0]['name']} exercises={courseParts[0]['exercises']} />
+      <Part partName={courseParts[1]['name']} exercises={courseParts[1]['exercises']} />
+      <Part partName={courseParts[2]['name']} exercises={courseParts[2]['exercises']} />
+  </div>
+  )
+}
+
+const Part = ({ partName, exercises }) => {
+  return (
+      <p>
+      {partName} {exercises}
+      </p>
+  )
+}
+
+const Course = ({ course }) => {
+    return (
+      <div>
+        <Header courseName={course.name} />
+        <Content courseParts={course.parts} />
+      </div>
+    )
+}
 
 const App = () => {
   const course = {
