55482be717567846a6435a8489aaf8eadd1f768c
Hemanth-D <hemanthh531@gmail.com>
2022-04-27 15:47:32 +0530

    part2 2.1: Modify App file to work, if we add/remove parts

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -8,10 +8,11 @@ const Content = ({ courseParts }) => {
   // console.log({ courseParts})
   return (
   // wrap the JSX expression in a fragment <> </> so that it has one parent element to the div 'root'
+  // JSX returns coursePart which is a single element inside courseParts list using the map function
   <div>
-      <Part partName={courseParts[0]['name']} exercises={courseParts[0]['exercises']} />
-      <Part partName={courseParts[1]['name']} exercises={courseParts[1]['exercises']} />
-      <Part partName={courseParts[2]['name']} exercises={courseParts[2]['exercises']} />
+    {courseParts.map(coursePart =>
+      <Part partName={coursePart['name']} exercises={coursePart['exercises']} key={coursePart['id']} />
+    )}
   </div>
   )
 }
