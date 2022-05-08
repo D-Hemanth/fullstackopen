7907128c6d599a0ebaa6a1fd0a59e08b084b42fc
Hemanth D <hemanthh531@gmail.com>
2022-05-08 18:53:59 +0530

    part2 2.6: Add new names to phonebook with button

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -7,6 +7,19 @@ const App = (props) => {
   const [newName, setNewName] = useState('')
 
 
+  // Add a new name to phonebook & prevent default action after form submission
+  const addName = (event) => {
+    event.preventDefault()
+    // console.log('submit button clicked', event.target)
+    const phonebookObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(phonebookObject));
+    // console.log('phonebook names list', persons)
+    setNewName('')
+  }
+
   // Handle addition of input element to react state
   const handleNameChange = (event) => {
     // console.log(event.target.value)
