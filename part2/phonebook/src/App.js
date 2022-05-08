505fc46d9a05c4fa88bb129cfb53104815b73c5e
Hemanth D <hemanthh531@gmail.com>
2022-05-08 18:46:18 +0530

    part2 2.6: Add boilerplate code for phonebook app

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = (props) => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
     </div>
-  );
+        <div>
+          <button type="submit">add</button>
+        </div>
+      <h2>Numbers</h2>
+    </div>
+  )
 }
 
-export default App;
+export default App
