4f039132edaba1241b804ce2852c265fc07645a0
Hemanth-D <hemanthh531@gmail.com>
2022-05-09 11:10:36 +0530

    part2 2.8: Allow users to add phone numbers

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -42,12 +42,9 @@ const App = (props) => {
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
-        <div>
-          name: <input onChange={handleNameChange} value={newName} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div>name: <input onChange={handleNameChange} value={newName} /></div>
+        <div>number: <input onChange={handleNumberChange} value={newNumber} /></div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       <div>
