2318575568b0ec0ad283b5a23d3f8b3180481731
Hemanth-D <hemanthh531@gmail.com>
2022-05-09 16:16:04 +0530

    part2 2.10: Fix the unique key warning for persons

--- part2/phonebook/src/components/Person.js
+++ part2/phonebook/src/components/Person.js
@@ -1,6 +1,6 @@
 const Person = ({ person }) => {
     return (
-        <div key={person.name}>{person.name} {person.number}</div>
+        <div>{person.name} {person.number}</div>
     )
 }
 
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -3,7 +3,7 @@ import Person from "./Person"
 const Persons = ({ persons }) => {
     return (
         <div>
-            {persons.map((person) => <Person person={person} />)}
+            {persons.map((person) => <Person person={person} key={person.name} />)}
         </div>
     )
 }
