03ea5acef82ff78d2523103e5396465f05bda5a2
Hemanth-D <hemanthh531@gmail.com>
2022-05-09 16:12:34 +0530

    part2 2.10: Add Person component import to persons

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,7 +1,9 @@
+import Person from "./Person"
+
 const Persons = ({ persons }) => {
     return (
         <div>
-            {persons.map((person) => <div key={person.name}>{person.name} {person.number}</div>)}
+            {persons.map((person) => <Person person={person} />)}
         </div>
     )
 }
