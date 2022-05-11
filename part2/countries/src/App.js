8d016ef3a2680fc302261b1ef3d7eb9667633ae1
Hemanth D <hemanthh531@gmail.com>
2022-05-11 19:43:10 +0530

    part2 2.12*: Add useffect & get data from rest api

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,6 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [allCountries, setAllCountries] = useState([])
+  const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    // console.log('effect')
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        console.log('promise fullfilled')
+        setAllCountries(response.data)
+        // console.log(response.data)
+      })
+  }, [])
 
 function App() {
   return (
