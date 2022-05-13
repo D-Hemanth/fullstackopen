129b41a6087a6540c13fc7863354d949d55bdf63
Hemanth D <hemanthh531@gmail.com>
2022-05-13 21:11:40 +0530

    part2 2.14*: Use useffect get openweather api data & setweather state to it

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -6,3 +6,27 @@ const Country = ({ country }) => {
     const [weather, setWeather] = useState([])
     const [isLoading, setLoading] = useState(true)
 
+        console.log('country for weather',country)
+        const weatherLocation = country.capital[0];   
+        const apiKey = process.env.REACT_APP_API_KEY;
+        console.log(apiKey)      
+        const params = {
+            q: weatherLocation, 
+            appid: apiKey,
+            units: 'metric'
+        };
+
+        // use useffect hooks to gather api response using axios get request & use 'then' for parsing the response
+        useEffect(() => {
+            axios
+            .get('https://api.openweathermap.org/data/2.5/weather?', { params })
+            .then(response => {
+                console.log('weather data gathered',response.data)
+                const apiResponse = response.data;
+                setWeather([apiResponse])
+                setLoading(false)
+            })
+            .catch((err) =>{
+                console.log(err);
+            })
+        },[]);
