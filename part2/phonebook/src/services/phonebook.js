822270e262d7899e30cad7cfe523efd06dba9456
Hemanth D <hemanthh531@gmail.com>
2022-05-16 21:19:30 +0530

    part 2 2.16: Add a post method to store input data on server in services

--- part2/phonebook/src/services/phonebook.js
+++ part2/phonebook/src/services/phonebook.js
@@ -6,3 +6,12 @@ const getAll = () => {
     const request = axios.get(baseurl)
     return request.then(response => response.data)
 }
+
+// do a axios post request to the url to send & store user input phonebook data in the backend server 
+const create = (phonebookObject) => {
+    const request = axios.post(baseurl, phonebookObject)
+    return request.then(response => response.data)
+}
+
+// export the getAll, create  as an object of any name i.e. here in app - phonebookService to use these methods in app
+export default { getAll, create }
