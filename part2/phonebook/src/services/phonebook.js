897da409b1f8f2d0629c399bf4ef48b1acb40bd4
Hemanth D <hemanthh531@gmail.com>
2022-05-16 21:18:11 +0530

    part 2 2.16: Add get method to gather data from server in services

--- part2/phonebook/src/services/phonebook.js
+++ part2/phonebook/src/services/phonebook.js
@@ -1,1 +1,8 @@
 import axios from "axios";
+const baseurl = 'http://localhost:3001/persons'
+
+// do a axios get request to the url to collect phonebook data from the server
+const getAll = () => {
+    const request = axios.get(baseurl)
+    return request.then(response => response.data)
+}
