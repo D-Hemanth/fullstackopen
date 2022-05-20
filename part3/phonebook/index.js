4f25bbadf559bc74c5b7cecac5a9e870d322af87
Hemanth-D <hemanthh531@gmail.com>
2022-05-20 16:18:28 +0530

    part3 3.4: Add delete route to delete a single phonebook entry

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -57,6 +57,13 @@ app.get('/api/persons/:id', (request, response) => {
 		}
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+		const id = Number(request.params.id)
+		persons = persons.filter(person => person.id !== id)
+
+		response.status(204).end()
+})
+
 // define a port to output the response received from the server 
 const PORT = 3001;
 app.listen(PORT, () => {
