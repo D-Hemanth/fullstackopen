7245224de3078dae7effb5031c94ae0cbe2a515f
Hemanth-D <hemanthh531@gmail.com>
2022-05-24 12:22:52 +0530

    part3 3.9: Complete phonebook backend step 9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,6 +2,30 @@ const { response, json } = require('express')
 const express = require('express')
 const app = express()
 
+let persons = [
+		{ 
+			"id": 1,
+			"name": "Arto Hellas", 
+			"number": "040-123456"
+		},
+		{ 
+			"id": 2,
+			"name": "Ada Lovelace", 
+			"number": "39-44-5323523"
+		},
+		{ 
+			"id": 3,
+			"name": "Dan Abramov", 
+			"number": "12-43-234345"
+		},
+		{ 
+			"id": 4,
+			"name": "Mary Poppendieck", 
+			"number": "39-23-6423122"
+		}
+]
+
+
 // Add morgan a HTTP request logger middleware for node.js using tiny configuration
 var morgan = require('morgan')
 
