51a74d7ae7cc3e674d1aa00e5df5025ca8b998c4
Hemanth-D <hemanthh531@gmail.com>
2022-05-24 12:22:17 +0530

    part3 3.9: Use cors & handle requests between 3000 front & 3001 backend

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -26,28 +26,9 @@ morgan.token('postData', (request) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
 
-let persons = [
-		{ 
-			"id": 1,
-			"name": "Arto Hellas", 
-			"number": "040-123456"
-		},
-		{ 
-			"id": 2,
-			"name": "Ada Lovelace", 
-			"number": "39-44-5323523"
-		},
-		{ 
-			"id": 3,
-			"name": "Dan Abramov", 
-			"number": "12-43-234345"
-		},
-		{ 
-			"id": 4,
-			"name": "Mary Poppendieck", 
-			"number": "39-23-6423122"
-		}
-]
+// cors middleware to use and allow for requests from all origins for connecting part2 phonebook frontend & part3 phonebook backend
+const cors = require('cors')
+app.use(cors())
 
 // get method to display all contacts in the phonebook
 app.get('/api/persons', (request, response) => {
