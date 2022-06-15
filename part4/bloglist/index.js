d4fb6f133e740add4a0a391470f97b27b252bfa6
Hemanth-D <hemanthh531@gmail.com>
2022-06-15 16:29:51 +0530

    part4 4.2: Define a server to listen to default PORT in index file

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -4,3 +4,7 @@ const config = require('./utils/config')
 
 const server = http.createServer(app)
 
+// define a port to output the response received from the server, the default port number is saved as config.js file
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
+})
