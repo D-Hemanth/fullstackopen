175723533246e49b02b559764093a32bc7e849d7
Hemanth-D <hemanthh531@gmail.com>
2022-06-15 14:55:11 +0530

    part4 4.1: Require packages for http, express, cors, mongoose, dotenv

---
+++ part4/bloglist/app.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const { response } = require('express')
+require('dotenv').config()
+
