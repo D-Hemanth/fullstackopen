cceee52176d443447a575bb8f2758c7354639a0a
Hemanth-D <hemanthh531@gmail.com>
2022-06-15 14:55:41 +0530

    part4 4.1: Add a schema which tells Mongoose how the blog objects are to be stored

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -6,3 +6,12 @@ const mongoose = require('mongoose')
 const { response } = require('express')
 require('dotenv').config()
 
+// Add a schema which tells Mongoose how the blog objects are to be stored in the database
+// Also add way for validating the format of the data before it is stored in the database using validation functionality of mongoose i.e. title, author, url, likes 
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+}) 
+
