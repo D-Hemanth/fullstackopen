c0010c519038f78c0d4f36f47acf5d84637473ca
Hemanth D <hemanthh531@gmail.com>
2022-06-18 20:55:30 +0530

    part4 4.3: Create a list_helper funcs file to deal with bloglist

---
+++ part4/bloglist/utils/list_helper.js
@@ -0,0 +1,6 @@
+// define a dummy function that receives an array of blog posts as a parameter and always returns the value 1
+const dummy = (blogs) => {
+    return 1
+}
+
+module.exports = { dummy }
