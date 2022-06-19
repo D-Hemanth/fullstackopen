222514c8dd5d7aa6193bab6f29f1fe058e85aae9
Hemanth D <hemanthh531@gmail.com>
2022-06-19 10:09:09 +0530

    part4 4.4: Complete helper functions and unit tests step 2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,6 +1,6 @@
 // define a dummy function that receives an array of blog posts as a parameter and always returns the value 1
 const dummy = (blogs) => {
-    return 1
+	return 1
 }
 
 // totalLikes function that receives a list of blog posts as a parameter. The function returns the total sum of likes in all of the blog posts
@@ -38,3 +38,5 @@ const totalLikes = (blogs) => {
 		return sum
 	}
 }
+
+module.exports = { dummy, totalLikes }
