cfc1a7f8dc7610da3d896acae72c52e2b3c876e5
Hemanth-D <hemanthh531@gmail.com>
2022-07-04 11:13:33 +0530

    part4 4.9*: Add transform method to blog model to format _id to id

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -9,5 +9,17 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 }) 
 
+// to remove unique id field _id & mongo versioning field __v from the frontend output
+// One way to format the objects returned by Mongoose is to modify the toJSON method of the schema
+// for get route When the response is sent in the JSON format, the toJSON method of each object in the array is called automatically by the JSON.stringify method
+// toJSON is responsible to define what data will be serialized(taken as input) to convert to json format
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 // public interface of the module is defined by setting a value to the module.exports variable here we have set the value to be the Blog model
 module.exports = mongoose.model('Blog', blogSchema)
