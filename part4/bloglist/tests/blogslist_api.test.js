80b321d84275872698b8d459873d75287f424c20
Hemanth-D <hemanthh531@gmail.com>
2022-07-04 11:15:15 +0530

    part4 4.9*: Add beforeEach method to clear & add blogs to database from test_helper file

--- part4/bloglist/tests/blogslist_api.test.js
+++ part4/bloglist/tests/blogslist_api.test.js
@@ -4,6 +4,16 @@ const app = require('../app')
 
 const api = supertest(app)
 
+// The database is cleared out at the beginning, Doing this we ensure that the database is in the same state before every test is run
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  // Promise.all method can be used for transforming an array of promises into a single promise, that will be fulfilled once every promise in the array passed to it as a parameter is resolved
+  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
 test('Blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
