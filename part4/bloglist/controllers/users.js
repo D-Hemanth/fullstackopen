2f213bd5b36363e2a4ae5dd119217cf7f55cbfb1
Hemanth-D <hemanthh531@gmail.com>
2022-07-12 12:26:28 +0530

    part4 4.15: Add post route for usersRouter to add new users into db

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -8,5 +8,22 @@ usersRouter.get('/', async (request, response) => {
   response.json(users)
 })
 
+// Add users into the database using post method of usersRouter
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  // When you are hashing your data the module will go through a series of rounds i.e. 2^rounds iterations of processing to give you a secure hash
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+  response.status(201).json(savedUser)
+})
 
 module.exports = usersRouter
