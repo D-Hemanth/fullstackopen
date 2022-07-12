04440c397cd25525d02438f4b02e15d5de1020bd
Hemanth-D <hemanthh531@gmail.com>
2022-07-12 12:20:34 +0530

    part4 4.15: Define a usersRouter in mongoose & take it to use in app.js

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -6,6 +6,9 @@ const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
 const mongoose = require('mongoose')
 
+// define a separate router for dealing with users 
+const usersRouter = require('./controllers/users')
+
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
 
@@ -18,5 +21,8 @@ app.use(express.json())
 
 // The blogsRouter we defined in ./controller/blogs is used if the URL of the request starts with '' & later /api/blogs are added to the request from blogs in controllers 
 app.use('', blogsRouter)
+
+// The usersRouter we defined in ./controller/users is used if the URL of the request starts with '/api/users'
+app.use('/api/users', usersRouter)
 
 module.exports = app
