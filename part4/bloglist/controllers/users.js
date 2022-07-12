3bd394331c3522859015f800363a5a8a32db4610
Hemanth-D <hemanthh531@gmail.com>
2022-07-12 12:25:53 +0530

    part4 4.15: Add get route for usersRouter to display all users in db

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -2,5 +2,11 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+// display all users in the database using get method of usersRouter
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
 
 module.exports = usersRouter
