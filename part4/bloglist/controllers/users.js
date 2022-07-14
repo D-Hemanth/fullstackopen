8af7b87e54374fd1ba4024bb0137aff05028bc79
Hemanth-D <hemanthh531@gmail.com>
2022-07-14 14:37:16 +0530

    part4 4.16*: check for unique username & password length min 3 characters

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -12,6 +12,17 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
+  // We can't give minLength in userSchema because that is passwordHash so don't use mongoose validator instead check using length like below
+  if(password.length < 3) {
+    return response.status(400).json({ error: `Password validation failed: Password is shorter than the minimum allowed length (3)` })
+  }
+
+  // validator for checking the uniqueness of a field, username in this case
+  const existingUser = await User.findOne({ username })
+  if(existingUser) {
+    return response.status(400).json({ error: 'username must be unique'})
+  }
+
   // When you are hashing your data the module will go through a series of rounds i.e. 2^rounds iterations of processing to give you a secure hash
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
