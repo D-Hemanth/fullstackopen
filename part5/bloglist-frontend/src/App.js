a9c383fd99292d3d56aaf828f428e2f55fb9157d
Hemanth-D <hemanthh531@gmail.com>
2022-07-21 15:14:54 +0530

    part5 5.1: Add react state variables for username, password, user

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -4,7 +4,12 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
+  // use Useeffect to add a side effect after rendering the component i.e. here we use it to get data from the server
+  // useEffect takes 2 parameters the effect function & the [] - array specifies how  often the effect function is run
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
