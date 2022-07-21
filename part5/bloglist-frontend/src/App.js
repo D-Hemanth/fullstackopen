c3b22cad988a021390f01bd1047a67ef88195103
Hemanth-D <hemanthh531@gmail.com>
2022-07-21 15:21:29 +0530

    part5 5.1: If user is logged in display username & blogs in list with authors

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -69,9 +69,10 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <p>{user.name} logged in</p>
+
+      { blogs.map(blog =>
+      <Blog key={blog.id} blog={blog} /> )}
     </div>
   )
 }
