977aad52b076baf6dbf0693657fbcc6fcf205c02
Hemanth-D <hemanthh531@gmail.com>
2022-07-25 22:56:15 +0530

    part5 5.8: Add update route to do axios put requests for blogs file in service

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -26,4 +26,9 @@ const create = async blogObject => {
   return response.data
 }
 
-export default { getAll, create, setToken }
+const update = async (likesBlogObject) => {
+  const response = await axios.put(`${baseUrl}/${likesBlogObject.id}`, likesBlogObject, config)
+  return response.data
+}
+
+export default { getAll, create, update, setToken }
