184a634cd49bc00aa6dd4c24acbbe7f76d678954
Hemanth-D <hemanthh531@gmail.com>
2022-07-25 19:59:09 +0530

    part5 5.7*:  Add css styling to the bloglist displayed using inline styles

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -12,7 +12,17 @@ const Blog = ({ blog }) => {
 
   const buttonLabel = visible ? 'hide' : 'view'
 
+  // add styles to the application using inline styles
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
   return (
+    <div style={blogStyle}>
       {blog.title} {blog.author}&nbsp; 
       <button onClick={toggleVisibility} blog={blog}>{buttonLabel}</button>
       <div style={showWhenVisible}>
