bd95945104fa085fdf59130d6fcb5fe45a318fd7
Hemanth-D <hemanthh531@gmail.com>
2022-07-25 19:58:06 +0530

    part5 5.7*:  Add visible states to toggle view/hide details of the blog

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,27 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  // visibility of the component is defined by an inline style rule, when the display property is set to none the component is not displayed
+  const showWhenVisible = {display: visible ? '' : 'none'}
+
+  const buttonLabel = visible ? 'hide' : 'view'
+
+  return (
+      {blog.title} {blog.author}&nbsp; 
+      <button onClick={toggleVisibility} blog={blog}>{buttonLabel}</button>
+      <div style={showWhenVisible}>
+        {blog.url}<br />
+        likes {blog.likes} <button>like</button><br />
+        {blog.user.name}<br />
+      </div>
   </div>  
 )
+}
 
 export default Blog
