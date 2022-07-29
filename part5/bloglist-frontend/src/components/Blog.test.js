bb1c7fb8a731c2cf8bc55d264bb705d2162832d2
Hemanth-D <hemanthh531@gmail.com>
2022-07-29 11:08:09 +0530

    part5 5.13: Add blog & user data for testing, render the blog component before each test

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,3 +1,28 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
+
+describe('<Blog/> component tests', () => {
+  let component
+
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Hemanth',
+    url: 'https://github.com/d-hemanth',
+    likes: 0,
+    user: [{
+      username: 'Hemanth',
+      name: 'Hemanth D',
+    }],
+  }
+
+  const user = {
+    username: 'Hemanth',
+    name: 'Hemanth D',
+  }
+
+  beforeEach(() => {
+    component = render(<Blog blog={blog} user={user} />)
+  })
