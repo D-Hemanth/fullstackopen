2502569245a8799e265bd2a036711602fa38b494
Hemanth-D <hemanthh531@gmail.com>
2022-08-01 22:09:20 +0530

    part5 5.17: After reseting db using beforeEach test login form is displayed by default

---
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -0,0 +1,15 @@
+describe('Blog app', function() {
+  // beforeEach block empties the server's database(mongodb) before tests are run
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.visit('http://localhost:3000')
+  })
+
+  // The test cases have been defined with the it method. Cypress borrowed these parts from the Mocha testing library it uses under the hood
+  it('Login form is shown', function() {
+    cy.contains('Log in to application')
+    cy.contains('username')
+    cy.contains('password')
+    cy.contains('login')
+  })
+})
