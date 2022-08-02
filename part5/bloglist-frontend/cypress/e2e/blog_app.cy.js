ed23ceb4b182c62fb92622cb806ef5624cbc0ef2
Hemanth-D <hemanthh531@gmail.com>
2022-08-02 20:24:30 +0530

    part5 5.22: Test that blog with most likes is ordered first during display

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -127,6 +127,22 @@ describe('Blog app', function() {
         cy.contains('First class tests').contains('view').click()
         cy.get('#remove-button').should('not.be.visible')
       })
+      it('check that the blogs are ordered according to likes with blog with the most likes being first', function() {
+        // First increase the likes of the two blogs already in db by giving aliases to each blog created so we can use it to refer to their likes count button
+        // and add a wait of 0.5 sec otherwise the click is too fast to be recognised as double click & it'll only show one click
+        cy.contains('A blog created by cypress').as('blog1')
+        cy.get('@blog1').contains('view').click()
+        cy.get('@blog1').contains('like').as('like1').click().wait(500).click()
+
+        cy.contains('First class tests').as('blog2')
+        cy.get('@blog2').contains('view').click()
+        cy.get('@blog2').contains('like').as('like2').click().wait(500).click().wait(500).click().wait(500)
+
+        // wrap - it yields the object passed into .wrap(). If the object is a promise, yield its resolved value.
+        cy.get('.blog').then(blogs => {
+          cy.wrap(blogs[0]).should('contain', 'First class tests')
+          cy.wrap(blogs[1]).should('contain', 'A blog created by cypress')
+        })
     })
   })
 })
