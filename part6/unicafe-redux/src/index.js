5bafc3b2715d9371c384a1301846d7eee4d480bd
Hemanth-D <hemanthh531@gmail.com>
2022-08-04 15:16:41 +0530

    part6 6.1: Complete unicafe revisited step1

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 import { createStore } from 'redux'
-import reducer from './reducer'
+import reducer from './reducers/counterReducer'
 
 const store = createStore(reducer)
 
