01fdb7d8a74e8875eb812587bc229ecaab5151b5
Hemanth-D <hemanthh531@gmail.com>
2022-08-06 18:28:28 +0530

    part6 6.3: Increment votes count by 1 for anecdote when vote click action is received by reducer

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -19,11 +19,24 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
+const anecdoteReducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
-
-  return state
+  switch (action.type) {
+    case 'NEW_ANECDOTE': return state.concat(action.data)
+    case 'TOGGLE_VOTE': {
+      const id = action.data.id
+      // console.log(id)
+      const anecdoteToChange = state.find(anecdote => anecdote.id === id)
+      // console.log(anecdoteToChange)
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1
+      }
+      return state.map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote)
+    }
+    default: return state
+  }
 }
 
-export default reducer
+export default anecdoteReducer
