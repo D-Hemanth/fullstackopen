d2c1524bbb70189183219b1d5deeea8e19c168be
Hemanth-D <hemanthh531@gmail.com>
2022-08-06 18:29:09 +0530

    part6 6.3: Complete anecdotes step1

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -7,6 +7,8 @@ import reducer from './reducers/anecdoteReducer'
 
 const store = createStore(reducer)
 
+// application is now defined as a child of a Provider component provided by the react redux library. 
+// The application's store is given to the Provider as its attribute store
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
