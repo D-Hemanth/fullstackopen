1cca3672d56a0709a7c91ef6485d3b57675c9448
Hemanth-D <hemanthh531@gmail.com>
2022-08-07 22:29:03 +0530

    part6 6.11: Add action creators setnotification & removenotification to reducers

--- part6/redux-anecdotes/src/reducers/notificatonReducer.js
+++ part6/redux-anecdotes/src/reducers/notificatonReducer.js
@@ -1,16 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = ['Initial state has 6 anecdotes']
+const initialState = null
 
 const notificationSlice = createSlice({
   name: 'notification',
   initialState,
   reducers: {
     setNotification(state, action) {
-
+      // console.log('setNotification action payload: ', action.payload)
+      return state = action.payload
     },
     removeNotification(state, action) {
-
+      // console.log('removeNotification action payload: ', action.payload)
+      return action.payload
     }
   }
 })
