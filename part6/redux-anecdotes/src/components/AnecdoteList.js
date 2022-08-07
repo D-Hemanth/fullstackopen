fe3df133a41dfb24748867f55e3825b82311b1b4
Hemanth-D <hemanthh531@gmail.com>
2022-08-07 17:52:21 +0530

    part6 6.9: Modify anecdoteList so that we mutate copy of state while sorting by votes

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -17,14 +17,20 @@ const Anecdote = ({anecdote, handleVoteIncrease}) => {
 
 const AnecdoteList = () => {
   // any React component can access the anecdotes stored in the store with the useSelector-hook,the function either searches for or selects data from the react-redux store
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.anecdote)
+  // console.log('anecdotes from useSelector', anecdotes)
   // useDispatch-hook provides any React component access to dispatch-function from the useDispatch -hook to send actions to react-redux store
   const dispatch = useDispatch()
 
+  // use the spread syntax to copy the state before mutating it using sort method to sort the anecdote list 
+  // by no. of votes in descending order and to avoid sort not a function error
+  const anecdotesToSort = [...anecdotes]
+  const sortedAnecdotes = anecdotesToSort.sort((a, b) => b.votes - a.votes)
+  // console.log('sorted anecdotes: ', sortedAnecdotes)
+
   return (
-    // use sort method to sort the anecdote list by no. of votes in descending order
     <ul>
-      {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
+      {sortedAnecdotes.map(anecdote =>
         <Anecdote 
           key={anecdote.id}
           anecdote={anecdote}
