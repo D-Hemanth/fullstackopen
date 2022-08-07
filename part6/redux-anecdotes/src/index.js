919e3d9a0525c7133ebd4690ea5edb9c09e24a54
Hemanth-D <hemanthh531@gmail.com>
2022-08-07 17:44:19 +0530

    part6 6.9: Add reducer as parameter to store which is passed to App using provider

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
-
-const store = createStore(reducer)
+import store from './utils/store'
 
 // application is now defined as a child of a Provider component provided by the react redux library. 
 // The application's store is given to the Provider as its attribute store
