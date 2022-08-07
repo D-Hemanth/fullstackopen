bfe324d3c024a75b528b10778c4744591d0a4388
Hemanth-D <hemanthh531@gmail.com>
2022-08-07 22:30:50 +0530

    part6 6.11: Complete better anecdotes step9

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -21,7 +21,7 @@ const Anecdote = ({anecdote, handleVoteIncrease}) => {
       </div>
       <div>
         has {anecdote.votes}
-        <button onClick={handleVoteIncrease}>vote</button>
+        <button onClick={() => voteIncrease(anecdote)}>vote</button>
       </div>
     </div>
   )
@@ -31,8 +31,7 @@ const AnecdoteList = () => {
   // any React component can access the anecdotes stored in the store with the useSelector-hook,the function either searches for or selects data from the react-redux store
   const anecdotes = useSelector(state => state.anecdote)
   // console.log('anecdotes from useSelector', anecdotes)
-  // useDispatch-hook provides any React component access to dispatch-function from the useDispatch -hook to send actions to react-redux store
-  const dispatch = useDispatch()
+
 
   // use the spread syntax to copy the state before mutating it using sort method to sort the anecdote list 
   // by no. of votes in descending order and to avoid sort not a function error
@@ -46,7 +45,6 @@ const AnecdoteList = () => {
         <Anecdote 
           key={anecdote.id}
           anecdote={anecdote}
-          handleVoteIncrease={() => dispatch(toggleIncreaseVote(anecdote.id))}
         />
       )}
     </div>
