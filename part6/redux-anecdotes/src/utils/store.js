cfa46a01698cf07e5e0e198dfc1a22bec8050a88
Hemanth-D <hemanthh531@gmail.com>
2022-08-08 17:17:33 +0530

    part6 6.12*: Add filter component to App & filter reducer to configurestore

--- part6/redux-anecdotes/src/utils/store.js
+++ part6/redux-anecdotes/src/utils/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import anecdoteReducer from '../reducers/anecdoteReducer'
 import notificatonReducer from '../reducers/notificatonReducer'
+import filterReducer from '../reducers/filterReducer'
 
 // create a store using configureStore function of redux-toolkit
 const store = configureStore({
   reducer: {
-    anecdote: anecdoteReducer,
-    notification: notificatonReducer
+    anecdotes: anecdoteReducer,
+    notification: notificatonReducer,
+    filter: filterReducer
   }
 })
 
