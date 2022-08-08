97386d6dc192469b45d84ea44abce72af74207fb
Hemanth-D <hemanthh531@gmail.com>
2022-08-08 21:47:49 +0530

    part6 6.13: Add dispatch action using useEffect to fetch data from backend on pageload

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -2,8 +2,20 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import { useEffect } from 'react'
+import anecdoteService from './services/anecdotes'
+import { useDispatch } from 'react-redux'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    // initialize the notes state based on the data received from the server fetch request & 
+    // to dispatch an action using the setNotes action creator for getting the note object array 
+    anecdoteService
+      .getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [dispatch])
+
   return (
     <div>
       <h2>Anecdotes</h2>
