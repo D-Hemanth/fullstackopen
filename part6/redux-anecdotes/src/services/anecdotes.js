08483010a58b6be448b6caf3d93129f641d78277
Hemanth-D <hemanthh531@gmail.com>
2022-08-08 22:03:25 +0530

    part6 6.14: Add axios post route to add data to the backend server

--- part6/redux-anecdotes/src/services/anecdotes.js
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -2,9 +2,17 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/anecdotes'
 
+// create a method which uses axios get method to fetch data from the backend
 const getAll = async () => {
   const response = await axios.get(baseUrl)
   return response.data
 }
 
-export default { getAll }
+// create a method which uses axios post method to add new data to backend
+const createNew = async (content) => {
+  const newAnecdoteObject = { content, votes: 0 }
+  const response = await axios.post(baseUrl, newAnecdoteObject)
+  return response.data
+}
+
+export default { getAll, createNew }
