977d3e9a78677ae0f7c8055c3b08a1cb34cf6c1d
Hemanth-D <hemanthh531@gmail.com>
2022-08-09 20:55:50 +0530

    part6 6.20: Modify filter to use connect func with mapDispatchToProps to send actions instead of useDispatch

--- part6/redux-anecdotes/src/components/Filter.js
+++ part6/redux-anecdotes/src/components/Filter.js
@@ -1,14 +1,13 @@
-import { useDispatch } from "react-redux"
+import { connect } from "react-redux"
 import { filterAnecdote } from "../reducers/filterReducer"
 
-const Filter = () => {
-  // useDispatch-hook provides any React component access to dispatch-function from the useDispatch -hook to send actions to react-redux store
-  const dispatch = useDispatch()
-
+const Filter = (props) => {
   const handleChange = (event) => {
     // input-field value is in variable event.target.value
     const filter = event.target.value
-    dispatch(filterAnecdote(filter))
+
+    // props contains the actions creators/ dispatch function from mapDispatchToProps function for dispatching actions
+    props.filterAnecdote(filter)
   }
   const style = {
     marginBottom: 10
@@ -21,4 +20,18 @@ const Filter = () => {
   )
 }
 
-export default Filter
+// Alternative mapDispatchToProps function, connect will invoke it by passing the dispatch-function as its parameter. 
+// The return value of the function is an object that defines a group of functions(here dispatch(filterAnecdote)) that get passed to the connected component as props
+// there are situations where the more complicated definition is necessary, like if the dispatched actions need to reference the props of the component -https://github.com/gaearon/redux-devtools/issues/250#issuecomment-186429931
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     filterAnecdote: value => {
+//       dispatch(filterAnecdote(value))
+//     }
+//   }
+// }
+
+// Since the component does not need to access the store's state, we can simply pass null as the first parameter to connect
+// connect function accepts a mapDispatchToProps function as its second parameter, which is a group of action creator/dispatch functions passed to the connected component as props, here destructuring object literal is used
+export default connect(null, { filterAnecdote })(Filter)
