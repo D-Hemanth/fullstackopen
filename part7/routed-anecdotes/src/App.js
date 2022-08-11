a00b6d03f1298bd75e3cf738e0ccb8aaf544b432
Hemanth-D <hemanthh531@gmail.com>
2022-08-11 20:45:09 +0530

    part7 7.1: Add Link  with different routes address path for different view components

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -6,9 +6,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link style={padding} to='/'>anecdotes</Link>
+      <Link style={padding} to='/create'>create new</Link>
+      <Link style={padding} to='/about'>about</Link>
     </div>
   )
 }
