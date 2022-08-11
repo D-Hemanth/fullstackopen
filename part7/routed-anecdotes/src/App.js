92e964a83284224b7fcc5573c71137c322dde9cd
Hemanth-D <hemanthh531@gmail.com>
2022-08-11 21:23:54 +0530

    part7 7.2: Add new Anecdote component to display single anecdote in desired format

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -19,6 +19,16 @@ const Menu = () => {
   )
 }
 
+const Anecdote = ({ anecdote }) => {
+  return( 
+    <div>
+      <h2>{anecdote.content} by {anecdote.author}</h2>
+      <p>has {anecdote.votes} votes</p>
+      <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+    </div>
+  )
+}
+
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
