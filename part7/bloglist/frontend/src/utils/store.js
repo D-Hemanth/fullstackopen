21108ca10249e69a0dd06f93225de0248f04fde3
Hemanth-D <hemanthh531@gmail.com>
2022-08-21 02:14:45 +0530

    part7 7.13: Add userReducer to redux store using cofigurestore

--- part7/bloglist/frontend/src/utils/store.js
+++ part7/bloglist/frontend/src/utils/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import blogReducer from '../reducers/blogReducer'
 import notificatonReducer from '../reducers/notificationReducer'
+import userReducer from '../reducers/userReducer'
 
 // create a store using configureStore function of redux-toolkit which enables us to use redux-thunk(i.e return functions from action creators), createSlice func
 const store = configureStore({
   reducer: {
     blogs: blogReducer,
+    user: userReducer,
     notification: notificatonReducer,
   },
 })
