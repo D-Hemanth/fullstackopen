938af44a854e2289c8e804f6705db14a84bfc2b7
Hemanth-D <hemanthh531@gmail.com>
2022-08-20 23:04:20 +0530

    part7 7.11: Add blogReducer into store as another reducer property field

--- part7/bloglist/frontend/src/utils/store.js
+++ part7/bloglist/frontend/src/utils/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import blogReducer from '../reducers/blogReducer'
 import notificatonReducer from '../reducers/notificationReducer'
 
 // create a store using configureStore function of redux-toolkit which enables us to use redux-thunk(i.e return functions from action creators), createSlice func
 const store = configureStore({
   reducer: {
+    blogs: blogReducer,
     notification: notificatonReducer,
   },
 })
