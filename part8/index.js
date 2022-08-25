52008e8aaf981761805389998b39311123c242be
Hemanth-D <hemanthh531@gmail.com>
2022-08-25 22:34:18 +0530

    part8 8.6: Complete adding a book

--- part8/index.js
+++ part8/index.js
@@ -90,6 +90,9 @@ let books = [
 ]
 
 // the schema for graphql queries & types of queries are defined inside the schema
+// the exclamation indicates it is a non-nullable string meaning the query to this field has to return a value & it can't be empty
+// you can provide named arguments to query fields by including them inside round brackets() & it should be have any name here (author: String!)
+// Mutation also has a return value of type Book so the details of the added book are returned if the operation is successful and if not, null.
 const typeDefs = gql`
   type Author {
     name: String!
