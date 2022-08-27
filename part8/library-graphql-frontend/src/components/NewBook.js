14c474c3b2d8a27d095ce984ba5a52182ffa206e
Hemanth-D <hemanthh531@gmail.com>
2022-08-27 19:04:33 +0530

    part8 8.10: Use useMutation to make mutation query & use refetchQueries to fetch updated server data & display it

--- part8/library-graphql-frontend/src/components/NewBook.js
+++ part8/library-graphql-frontend/src/components/NewBook.js
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { ADD_BOOK, ALL_AUTHORS, ALL_BOOKS } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -7,6 +9,10 @@ const NewBook = (props) => {
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
 
+  const [createBook] = useMutation(ADD_BOOK, {
+    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }],
+  })
+
   if (!props.show) {
     return null
   }
@@ -14,7 +20,11 @@ const NewBook = (props) => {
   const submit = async (event) => {
     event.preventDefault()
 
-    console.log('add book...')
+    // use Number in input element to convert the published value to int before saving to react state
+    // console.log('int converted value of published', published)
+
+    // use createBook query from useMutation to addBoook to gql server
+    createBook({ variables: { title, published, author, genres } })
 
     setTitle('')
     setPublished('')
@@ -50,7 +60,7 @@ const NewBook = (props) => {
           <input
             type="number"
             value={published}
-            onChange={({ target }) => setPublished(target.value)}
+            onChange={({ target }) => setPublished(Number(target.value))}
           />
         </div>
         <div>
