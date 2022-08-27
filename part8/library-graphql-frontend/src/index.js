d992ac4aaa9661250470f700652ba290d2c14018
Hemanth-D <hemanthh531@gmail.com>
2022-08-27 13:11:12 +0530

    part8 8.8: Create apollo client object using httplink, InMemoryCache, add apolloProvider to use client everywhere

--- part8/library-graphql-frontend/src/index.js
+++ part8/library-graphql-frontend/src/index.js
@@ -2,4 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client'
+
+// create a new client object using ApolloClient, which is then used to send a query to the server
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  }),
+})
+
+//  client can be made accessible for all components of the application by wrapping the App component with ApolloProvider
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
