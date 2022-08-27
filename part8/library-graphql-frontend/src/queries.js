5c2e1f16b490c9485fce9f043034d1ea0b676961
Hemanth-D <hemanthh531@gmail.com>
2022-08-27 14:34:26 +0530

    part8 8.9: Add query allBooks to get all books info from gql library-backend

--- part8/library-graphql-frontend/src/queries.js
+++ part8/library-graphql-frontend/src/queries.js
@@ -1,11 +1,24 @@
 import { gql } from '@apollo/client'
 
+// add a query display all the authors in the graphql server
 export const ALL_AUTHORS = gql`
   query {
     allAuthors {
       name
       born
       bookCount
+      id
+    }
+  }
+`
+
+// add a query display all books in the graphql server
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      author
+      published
       id
     }
   }
