66c8af18e732a86ab1bce6f4bb51c34848178432
Hemanth-D <hemanthh531@gmail.com>
2022-08-27 14:35:07 +0530

    part8 8.9: Use useQuery hook to query gql server for all books info, if data is loaded display it

--- part8/library-graphql-frontend/src/components/Books.js
+++ part8/library-graphql-frontend/src/components/Books.js
@@ -1,9 +1,20 @@
+import { useQuery } from '@apollo/client'
+import { ALL_BOOKS } from '../queries'
 const Books = (props) => {
+  // the hook function useQuery makes the query it receives as a parameter, It returns an object with multiple fields { loading, error, data: queryType }
+  const result = useQuery(ALL_BOOKS)
+  // console.log('allBooks query result', result)
+
   if (!props.show) {
     return null
   }
 
-  const books = []
+  // The field loading is true if the query has not received a response yet
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  const books = result.data.allBooks
 
   return (
     <div>
