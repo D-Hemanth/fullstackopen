4f5d0d77bcc01b686bbdc74ef3a9673a056146c2
Hemanth-D <hemanthh531@gmail.com>
2022-08-31 18:44:50 +0530

    part8 8.17: Complete listing books

--- part8/library-graphql-backend/index.js
+++ part8/library-graphql-backend/index.js
@@ -1,4 +1,9 @@
-const { ApolloServer, gql, UserInputError, AuthenticationError } = require('apollo-server')
+const {
+  ApolloServer,
+  gql,
+  UserInputError,
+  AuthenticationError,
+} = require('apollo-server')
 const config = require('./utils/config')
 const jwt = require('jsonwebtoken')
 
@@ -12,7 +17,8 @@ const JWT_SECRET = config.SECRET
 const MONGODB_URI = config.MONGODB_URI
 console.log('connecting to', MONGODB_URI)
 
-mongoose.connect(MONGODB_URI)
+mongoose
+  .connect(MONGODB_URI)
   .then(() => {
     console.log('connected to MongoDB')
   })
@@ -79,7 +85,7 @@ const resolvers = {
     bookCount: async () => Book.collection.countDocuments(),
     authorCount: async () => Author.collection.countDocuments(),
     // Implement query allBooks, which returns the details of all books, if author argument is not given else return all books written by that author
-    allBooks: (root, args) => {
+    allBooks: async (root, args) => {
       // if both author, genre are not given in argument parameter then return all books
       if (!args.author && !args.genre) {
         return await Book.find({}).populate('author')
@@ -89,29 +95,30 @@ const resolvers = {
       let authorWrittenBooks = await Book.find({}).populate('author')
 
       if (args.author)
-        authorWrittenBooks = await authorWrittenBooks.filter((book) => book.author.name === args.author)
-
+        authorWrittenBooks = await authorWrittenBooks.filter(
+          (book) => book.author.name === args.author
+        )
       // use the books response from authorWrittenBooks to again filter by genre if genre parameter is given to return authorBooksGenreBased else return authorWrittenBooks
       if (args.genre) {
         // The $in operator selects the documents where the value of a field(here genres) equals any value in the specified array
-        const authorBooksGenreBased = await authorWrittenBooks.find({ genres: { $in: [args.genre] } })
+        const authorBooksGenreBased = await authorWrittenBooks.find({
+          genres: { $in: [args.genre] },
+        })
         return authorBooksGenreBased
       }
 
       return authorWrittenBooks
     },
     // Implement query allAuthors, which returns the details of all authors & include a field bookCount containing the number of books the author has written
-    allAuthors: async () => {
-      await Author.find({})
-    },
+    allAuthors: async () => await Author.find({}),
     me: (root, args, context) => {
       return context.currentUser
-    }
+    },
   },
   Author: {
-    bookCount: (root) => {
+    bookCount: async (root) => {
       // find the books with author field having the id same as root.id i.e. current author id whose bookCount we're tallying
-      const books = await Book.find({ author: root.id})
+      const books = await Book.find({ author: root.id })
       return books.length
     },
   },
@@ -119,12 +126,12 @@ const resolvers = {
     addBook: async (root, args, context) => {
       // before editing persons verify that the user is logged-in using context parameter of resolver
       const currentUser = context.currentUser
-      if(!currentUser) {
+      if (!currentUser) {
         throw new AuthenticationError('Not Authenticated')
       }
 
-      // author name being too short(<4) then mongoose database validation error handling 
-      if(args.author.length < 4) {
+      // author name being too short(<4) then mongoose database validation error handling
+      if (args.author.length < 4) {
         throw new UserInputError('Author name is less than 4 characters', {
           invalidArgs: args.author,
         })
@@ -132,7 +139,7 @@ const resolvers = {
 
       // If the author is not yet saved to the server, a new author is added to the system. The birth years of authors are not saved to the server yet
       const existingAuthor = await Author.findOne({ name: args.author })
-      if(!existingAuthor) {
+      if (!existingAuthor) {
         const author = new Author({ name: args.author, born: null })
         // For handling possible validation errors in the schema, we must add an error-handling try/catch block to the save method
         try {
@@ -144,11 +151,11 @@ const resolvers = {
         }
       }
 
-      // book title already exists then mongoose database validation error handling 
+      // book title already exists then mongoose database validation error handling
       const existingBook = await Book.findOne({ title: args.title })
-      if(existingBook) {
+      if (existingBook) {
         throw new UserInputError('Book already exists', {
-          invalidArgs: args.title
+          invalidArgs: args.title,
         })
       }
 
@@ -165,15 +172,15 @@ const resolvers = {
       return book
     },
     // Implement mutation editAuthor, which can be used to set a birth year for an author
-    editAuthor: (root, args, context) => {
+    editAuthor: async (root, args, context) => {
       // before editing persons verify that the user is logged-in using context parameter of resolver
       const currentUser = context.currentUser
       if (!currentUser) {
-        throw new AuthenticationError("not authenticated")
+        throw new AuthenticationError('not authenticated')
       }
 
       const author = await Author.find({ name: args.name })
-      if(!author) {
+      if (!author) {
         return null
       }
 
@@ -184,26 +191,28 @@ const resolvers = {
         await author.save()
       } catch (error) {
         throw new UserInputError(error.message, {
-          invalidArgs: args
+          invalidArgs: args,
         })
       }
       return author
     },
     createUser: async (root, args) => {
-      const user = new User({ username: args.username, favouriteGenre: args.favouriteGenre })
+      const user = new User({
+        username: args.username,
+        favouriteGenre: args.favouriteGenre,
+      })
 
-      return user.save()
-        .catch(error => {
-          // catch user input errors using UserInputError method
-          throw new UserInputError(error.message, {
-            invalidArgs: args,
-          })
+      return user.save().catch((error) => {
+        // catch user input errors using UserInputError method
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
         })
+      })
     },
-    login: async(root, args) => {
+    login: async (root, args) => {
       const user = await User.findOne({ username: args.username })
 
-      if(!user || args.password !== 'secret') {
+      if (!user || args.password !== 'secret') {
         throw new UserInputError('wrong credentials')
       }
 
@@ -225,12 +234,12 @@ const server = new ApolloServer({
   context: async ({ req }) => {
     // The object returned by context is given to all resolvers as their third parameter. Context is the right place to do things which are shared by multiple resolvers, like user identification
     const auth = req ? req.headers.authorization : null
-    if(auth && auth.toLocaleLowerCase().startsWith('bearer ')) {
+    if (auth && auth.toLocaleLowerCase().startsWith('bearer ')) {
       const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET)
       const currentUser = await User.findById(decodedToken.id)
       return { currentUser }
     }
-  }
+  },
 })
 
 server.listen().then(({ url }) => {
