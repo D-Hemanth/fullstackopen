8c7f809da1e1b4856acbd74c41a63e87d891fc8b
Hemanth-D <hemanthh531@gmail.com>
2022-08-31 23:59:36 +0530

    part8 8.18: Setcontext to apollo client using authLink & httplink with token from local storage

--- part8/library-graphql-frontend/src/index.js
+++ part8/library-graphql-frontend/src/index.js
@@ -8,13 +8,27 @@ import {
   HttpLink,
   InMemoryCache,
 } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
+// creating new persons requires that a valid user token is sent with the request so we have to change the ApolloClient object by also sending authLink along with HttpLink
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('library-user-token')
+  // console.log('token in authLink', token)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `bearer ${token}` : null,
+    },
+  }
+})
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
 // create a new client object using ApolloClient, which is then used to send a query to the server
+// The link parameter given to the client object defines how apollo connects to the server. Here, the normal httpLink connection is modified so that the request's authorization header contains the token of the user who logged-in & gql server address
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: 'http://localhost:4000',
-  }),
+  link: authLink.concat(httpLink),
 })
 
 //  client can be made accessible for all components of the application by wrapping the App component with ApolloProvider
