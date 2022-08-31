2eddeb56663a01f806b50ebe080a7eea9b7d45a7
Hemanth-D <hemanthh531@gmail.com>
2022-09-01 00:01:20 +0530

    part8 8.18: Modify App file to display login form if user not logged-in & show addBook view after login

--- part8/library-graphql-frontend/src/App.js
+++ part8/library-graphql-frontend/src/App.js
@@ -1,10 +1,56 @@
 import { useState } from 'react'
+import { useApolloClient } from '@apollo/client'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [errorMessage, setErrorMessage] = useState(null)
+  // When a user is logged in, token state will contain a user token
+  const [token, setToken] = useState(null)
+
+  // some queries might have fetched data to cache, We can reset the cache after user logout using the resetStore method of an Apollo 'client' object. The client can be accessed with the useApolloClient hook
+  const client = useApolloClient()
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  // We can reset the cache after user logout using the resetStore method of an Apollo 'client' object
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <div>
+          <button onClick={() => setPage('authors')}>authors</button>
+          <button onClick={() => setPage('books')}>books</button>
+          <button onClick={() => setPage('login')}>login</button>
+        </div>
+
+        <Authors show={page === 'authors'} setError={notify} />
+
+        <Books show={page === 'books'} />
+
+        <LoginForm
+          show={page === 'login'}
+          setPage={setPage}
+          setToken={setToken}
+          setError={notify}
+        />
+      </div>
+    )
+  }
 
   return (
     <div>
@@ -12,15 +58,23 @@ const App = () => {
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
+        <button onClick={logout}>logout</button>
       </div>
 
-      <Authors show={page === 'authors'} />
+      <Authors show={page === 'authors'} setError={notify} />
 
       <Books show={page === 'books'} />
 
-      <NewBook show={page === 'add'} />
+      <NewBook show={page === 'add'} setError={notify} />
     </div>
   )
+}
+
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null
+  }
+  return <div style={{ color: 'red' }}>{errorMessage}</div>
 }
 
 export default App
