15179d80c14fa8ea922f39ee3dd7a66009f5c093
Hemanth-D <hemanthh531@gmail.com>
2022-09-13 22:26:11 +0530

    part9 9.3: Complete command line

--- part9/typescript-demo-bmi/exerciseCalculator.ts
+++ part9/typescript-demo-bmi/exerciseCalculator.ts
@@ -30,65 +30,66 @@ const parseExerciseArguments = (args: Array<string>): ExerciseParams => {
   }
 }
 
+// TypeScript's Interface object type keyword, which is one way to define the "shape" an object should have
 interface ExercisesResult {
-  periodLength: number,
-  trainingDays: number, 
-  success: boolean,
-  rating: number,
-  ratingDescription: string,
-  target: number,
-  average: number,
+  periodLength: number
+  trainingDays: number
+  success: boolean
+  rating: number
+  ratingDescription: string
+  target: number
+  average: number
 }
 
-const calculateExercises = (targetHoursValue: number, dailyExerciseHours: Array<number>): ExercisesResult => {
-  if(targetHoursValue < 1) throw new Error('original taget exercise hours is below zero!');
+const calculateExercises = (
+  targetHoursValue: number,
+  dailyExerciseHours: Array<number>
+): ExercisesResult => {
+  if (targetHoursValue < 1)
+    throw new Error('original target exercise hours is below one!')
 
-  const periodLength = dailyExerciseHours.length;
-  const trainingDays = dailyExerciseHours.filter(hour => hour > 0).length;
-  const target = Number(targetHoursValue);
-  const totalHours = dailyExerciseHours.reduce((sum, currentHour) => sum + currentHour, 0);
-  const average = totalHours / periodLength;
-  const success = (average >= target) ? true : false;
+  const periodLength = dailyExerciseHours.length
+  const trainingDays = dailyExerciseHours.filter((hour) => hour > 0).length
+  const target = Number(targetHoursValue)
+  const totalHours = dailyExerciseHours.reduce(
+    (sum, currentHour) => sum + currentHour,
+    0
+  )
+  const average = totalHours / periodLength
+  const success = average >= target ? true : false
 
-  let rating;
-  let ratingDescription;
-  if(average < (target / 3)) {
-    rating = 1;
-    ratingDescription = 'You have failed to reach daily exercise hours, keep trying!';
+  let rating
+  let ratingDescription
+  if (average < target / 3) {
+    rating = 1
+    ratingDescription =
+      'You have failed to reach daily exercise hours, keep trying!'
   } else if (average > target) {
-    rating = 3;
-    ratingDescription = 'Great going you\'ve reached your daily exercises hours goal!';
+    rating = 3
+    ratingDescription =
+      "Great going you've reached your daily exercises hours goal!"
   } else {
-    rating = 2;
-    ratingDescription = 'Not too bad but could be better';
+    rating = 2
+    ratingDescription = 'Not too bad but could be better'
   }
-
-  console.log({
-    periodLength: periodLength,
-    trainingDays: trainingDays, 
-    success: success,
-    rating: rating,
-    ratingDescription: ratingDescription, 
-    target: target,
-    average: average,
-  })
 
   return {
     periodLength: periodLength,
-    trainingDays: trainingDays, 
+    trainingDays: trainingDays,
     success: success,
     rating: rating,
-    ratingDescription: ratingDescription, 
+    ratingDescription: ratingDescription,
     target: target,
     average: average,
-  };
+  }
 }
 
 try {
-  calculateExercises(2, [3, 0, 2, 4.5, 0, 3, 1]);
+  const { target, dailyTrainingHours } = parseExerciseArguments(process.argv)
+  console.log(calculateExercises(target, dailyTrainingHours))
 } catch (error: unknown) {
-  let errorMessage = 'Something went wrong';
-  if(error instanceof Error) {
+  let errorMessage = 'Something went wrong'
+  if (error instanceof Error) {
     errorMessage += ' Error: ' + error.message
   }
   console.log(errorMessage)
