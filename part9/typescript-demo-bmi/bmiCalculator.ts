cd55c3eccd761ceb8d6b69af17abfb8e22a2562e
Hemanth-D <hemanthh531@gmail.com>
2022-09-13 22:24:21 +0530

    part9 9.3: Modify bmiCalculator to use command line args for bmi calcs

--- part9/typescript-demo-bmi/bmiCalculator.ts
+++ part9/typescript-demo-bmi/bmiCalculator.ts
@@ -1,3 +1,24 @@
+// TypeScript's Interface object type keyword, which is one way to define the "shape" an object should have
+interface BodyMassParams {
+  height: number
+  mass: number
+}
+
+// the args has only 4 arguments because when we execute the script to run this, > ts-node bmiCalculator.ts "180" "91" , will be run which has 4 params
+const parseBmiArguments = (args: Array<string>): BodyMassParams => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length > 4) throw new Error('Too many arguments')
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      mass: Number(args[3]),
+    }
+  } else {
+    throw new Error('Provided values for height & mass were not numbers')
+  }
+}
+
 const calculateBmi = (height: number, mass: number): string => {
   if (height == 0) throw new Error("Can't divide by 0!")
 
@@ -23,7 +44,9 @@ const calculateBmi = (height: number, mass: number): string => {
 }
 
 try {
-  console.log(calculateBmi(180, 74))
+  // We can get the calculateBmi to work with command-line parameters using process.argv
+  const { height, mass } = parseBmiArguments(process.argv)
+  console.log(calculateBmi(height, mass))
 } catch (error: unknown) {
   let errorMessage = 'Something went wrong.'
   if (error instanceof Error) {
