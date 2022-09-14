c6593132d1c8d6dcab23db44b700d4afa625c11e
Hemanth-D <hemanthh531@gmail.com>
2022-09-14 21:32:19 +0530

    part9 9.7: Complete webexercises

--- part9/typescript-demo-bmi/bmiCalculator.ts
+++ part9/typescript-demo-bmi/bmiCalculator.ts
@@ -1,41 +1,47 @@
 // TypeScript's Interface object type keyword, which is one way to define the "shape" an object should have
 interface BodyMassParams {
-  heightInCm: number
-  weightInKg: number
+  heightInCm: number;
+  weightInKg: number;
 }
 
 // the args has only 4 arguments because when we execute the script to run this, > ts-node bmiCalculator.ts "180" "91" , will be run which has 4 params
-export const parseBmiArguments = (height: number, weight: number): BodyMassParams => {
+export const parseBmiArguments = (
+  height: number,
+  weight: number
+): BodyMassParams => {
   if (!isNaN(height) && !isNaN(weight)) {
     return {
       heightInCm: height,
       weightInKg: weight,
-    }
+    };
   } else {
-    throw new Error('Provided values for height & mass were not numbers!')
+    throw new Error('Provided values for height & mass were not numbers!');
   }
-}
+};
 
-export const calculateBmi = (heightInCm: number, weightInKg: number): string => {
-  if (heightInCm == 0) throw new Error("Can't divide by 0!")
+export const calculateBmi = (
+  heightInCm: number,
+  weightInKg: number
+): string => {
+  if (heightInCm == 0) throw new Error("Can't divide by 0!");
 
-  const bmi = (weightInKg * 100 * 100) / (heightInCm * heightInCm)
+  const bmi = (weightInKg * 100 * 100) / (heightInCm * heightInCm);
 
   if (bmi < 16.0) {
-    return 'Underweight (Severe thinness)'
+    return 'Underweight (Severe thinness)';
   } else if (bmi > 16.0 && bmi < 16.9) {
-    return 'Underweight (Moderate thinness)'
+    return 'Underweight (Moderate thinness)';
   } else if (bmi > 17.0 && bmi < 18.4) {
-    return 'Underweight (Mild thinness)'
+    return 'Underweight (Mild thinness)';
   } else if (bmi > 18.5 && bmi < 24.9) {
-    return 'Normal (Healthy weight)'
+    return 'Normal (Healthy weight)';
   } else if (bmi > 25.0 && bmi < 29.9) {
-    return 'Overweight (Pre-obese)'
+    return 'Overweight (Pre-obese)';
   } else if (bmi > 30.0 && bmi < 34.9) {
-    return 'Obese (Class I)'
+    return 'Obese (Class I)';
   } else if (bmi > 35.0 && bmi < 39.9) {
-    return 'Obese (Class II)'
+    return 'Obese (Class II)';
   } else {
-    return 'Obese (Class III)'
+    return 'Obese (Class III)';
   }
-}
+};
