41028ee170f76cf0df68f172edfcdc06391158ad
Hemanth-D <hemanthh531@gmail.com>
2022-09-14 10:56:16 +0530

    part9 9.4: Complete express

--- part9/typescript-demo-bmi/index.ts
+++ part9/typescript-demo-bmi/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 const app = express();
 
+//  to get rid of an unused variable error, you can prefix it with an underscore to inform the compiler you have thought about it, Let's rename the req variable to _req
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 })
