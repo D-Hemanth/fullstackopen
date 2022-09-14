baacfdf9a8e514aa957b15a93fe51133ef8f64aa
Hemanth-D <hemanthh531@gmail.com>
2022-09-14 21:30:57 +0530

    part9 9.7: Add exercises post route to send daily exercises & target to get calculated exercises detail

--- part9/typescript-demo-bmi/index.ts
+++ part9/typescript-demo-bmi/index.ts
@@ -1,35 +1,69 @@
 import express from 'express';
 import { parseBmiArguments, calculateBmi } from './bmiCalculator';
+import {
+  calculateExercises,
+  parseExerciseArguments,
+} from './exerciseCalculator';
 const app = express();
 
+// In order to access the POST data easily, we need the help of the express json-parser that is taken to use with following command
+app.use(express.json());
+
 //  to get rid of an unused variable error, you can prefix it with an underscore to inform the compiler you have thought about it, Let's rename the req variable to _req
-app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!');
+app.get('/hello', (_request, response) => {
+  response.send('Hello Full Stack!');
 });
 
 // add a bmi query route to get the bmi of person by going to the link - http://localhost:3002/bmi?height=180&weight=72
-app.get('/bmi', (req, res) => {
+app.get('/bmi', (request, response) => {
   // get the height & weight from the query string of the url address
-  const height = Number(req.query.height);
-  const weight = Number(req.query.weight);
+  const height = Number(request.query.height);
+  const weight = Number(request.query.weight);
 
   if (!height || !weight) {
-    res.status(400);
-    res.send({ error: 'malformatted parameters' });
+    response.status(400);
+    response.send({ error: 'malformatted parameters' });
   } else {
     try {
       const { heightInCm, weightInKg } = parseBmiArguments(height, weight);
 
       const bmi = calculateBmi(heightInCm, weightInKg);
-      res.send({
+      response.send({
         weight: weightInKg,
         height: heightInCm,
         bmi: bmi,
       });
     } catch (error) {
-      res.status(400);
-      res.send({ error: error.message });
+      response.status(400);
+      response.send({ error: error.message });
     }
+  }
+});
+
+// add exercise calculator route using post method to get the exercise details of person, you can test this using restclient
+app.post('/exercises', (request, response) => {
+  const dailyExercises = request.body.daily_exercises;
+  const dailyTarget = request.body.target;
+
+  if (!dailyExercises || !dailyTarget) {
+    response.status(400);
+    response.send({ error: 'daily_exercises or target Parameters Missing' });
+  }
+
+  try {
+    const { target, dailyTrainingHours } = parseExerciseArguments(
+      dailyTarget,
+      dailyExercises
+    );
+
+    const exercisesCalculatorResponse = calculateExercises(
+      target,
+      dailyTrainingHours
+    );
+    response.send(exercisesCalculatorResponse);
+  } catch (error) {
+    response.status(400);
+    response.send({ error: error.message });
   }
 });
 
