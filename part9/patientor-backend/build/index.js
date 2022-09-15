fe8024bcc258ce6c84f80742df9713fad4178362
Hemanth-D <hemanthh531@gmail.com>
2022-09-15 16:33:34 +0530

    part9 9.9: Complete patientor backend step2

--- part9/patientor-backend/build/index.js
+++ part9/patientor-backend/build/index.js
@@ -5,8 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+const cors_1 = __importDefault(require("cors"));
 app.use(express_1.default.json());
-const PORT = 3000;
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use((0, cors_1.default)());
+const PORT = 3001;
 app.get("/api/ping", (_request, response) => {
     console.log("someone pinged here");
     response.send("pong");
