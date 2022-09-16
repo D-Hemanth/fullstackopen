d4f814841ffd9470a0af211db07eb73f3365b09f
Hemanth-D <hemanthh531@gmail.com>
2022-09-16 17:10:14 +0530

    part9 9.10: Complete Patientor backend step3

---
+++ part9/patientor-backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import diagnosesRouter from "./routes/diagnoses";
+const app = express();
+import cors from "cors";
+
+app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+
+const PORT = 3001;
+
+app.get("/api/ping", (_request, response) => {
+  console.log("someone pinged here");
+  response.send("pong");
+});
+
+// We'll route all requests to prefix /api/diagnoses to that specific router, here diagnosesRouter
+app.use("/api/diagnoses", diagnosesRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
