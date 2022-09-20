80c5a5f554749f39b1600db675831953c0773e34
Hemanth-D <hemanthh531@gmail.com>
2022-09-20 22:33:20 +0530

    part9 9.13: Add validation to post route /api/patients when adding new patients

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -1,24 +1,30 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from 'express';
 import patientsService from '../services/patientsService';
+import toNewPatientEntry from '../utils';
 
 const router = express.Router();
 
+// get route to get all the patient entries without the sensitive info like ssn
 router.get('/', (_request, response) => {
   response.send(patientsService.getNonSensitiveEntries());
 });
 
+// post route to add new patient entries
 router.post('/', (request, response) => {
-  const { name, dateOfBirth, ssn, gender, occupation } = request.body;
-  const newPatientEntry = patientsService.addPatient({
-    name,
-    dateOfBirth,
-    ssn,
-    gender,
-    occupation,
-  });
+  try {
+    // Use function toNewPatientEntry that receives the request body as a parameter and returns a properly-typed NewPatientEntry object
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const newPatientEntry = toNewPatientEntry(request.body);
 
-  response.json(newPatientEntry);
+    const addedPatientEntry = patientsService.addPatient(newPatientEntry);
+    response.json(addedPatientEntry);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    response.status(400).send(errorMessage);
+  }
 });
 
 export default router;
