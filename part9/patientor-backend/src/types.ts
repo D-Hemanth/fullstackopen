e4b1d816053a17494e56aeeabb5f9232276e706b
Hemanth-D <hemanthh531@gmail.com>
2022-09-20 20:14:25 +0530

    part9 9.12: Add NewPatientEntry type with all fields as patientEntry except id using omit

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,3 +1,5 @@
+// TypeScript's Interface object type keyword, which is one way to define the "shape" an object should have
+// Note that, if we want to be able to save entries without a certain field, e.g. comment, we could set the type of the field as optional by adding ? to the type declaration
 export interface DiagnosesEntry {
   code: string;
   name: string;
@@ -13,4 +15,8 @@ export interface PatientsEntry {
   occupation: string;
 }
 
-export type NonSensitivePatientsEntry = Omit<PatientsEntry, "ssn">;
+// In this case, we want to exclude only one field(ssn), so it would be even better to use the Omit utility type, which we can use to declare which fields to exclude:
+export type NonSensitivePatientsEntry = Omit<PatientsEntry, 'ssn'>;
+
+// create a new type, newPatientEntry, for an entry that hasn't been saved yet
+export type NewPatientEntry = Omit<PatientsEntry, 'id'>;
