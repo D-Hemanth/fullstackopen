e29576b31702c46c4c0f4319a28fa87c07fa87e3
Hemanth-D <hemanthh531@gmail.com>
2022-09-22 22:29:14 +0530

    part9 9.14: Modify the App file by creating separate components for header, content, total

--- part9/courseinfo/src/App.tsx
+++ part9/courseinfo/src/App.tsx
@@ -17,20 +17,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{courseName}</h1>
-      <p>
-        {courseParts[0].name} {courseParts[0].exerciseCount}
-      </p>
-      <p>
-        {courseParts[1].name} {courseParts[1].exerciseCount}
-      </p>
-      <p>
-        {courseParts[2].name} {courseParts[2].exerciseCount}
-      </p>
-      <p>
-        Number of exercises{' '}
-        {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
-      </p>
+      <Header courseName={courseName} />
+      <Content courseParts={courseParts} />
+      <Total courseParts={courseParts} />
     </div>
   );
 };
