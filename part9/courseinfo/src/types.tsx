e3f85ff05a49580f72fcbad1f149754bdef5c1c6
Hemanth-D <hemanthh531@gmail.com>
2022-09-22 22:28:07 +0530

    part9 9.14: Create types file with inteface CourseParts with fields name, exerciseCounts

---
+++ part9/courseinfo/src/types.tsx
@@ -0,0 +1,5 @@
+// TypeScript's Interface object type keyword, which is one way to define the "shape" an object should have
+export interface CourseParts {
+  name: string;
+  exerciseCount: number;
+}
