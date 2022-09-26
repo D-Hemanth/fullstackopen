2da74ef5d5bcac1ad5775163d739b61bab3afc0a
Hemanth-D <hemanthh531@gmail.com>
2022-09-26 22:12:10 +0530

    part9 9.15: Modify types file to use extend & union type to define interfaces & types

--- part9/courseinfo/src/types.tsx
+++ part9/courseinfo/src/types.tsx
@@ -1,5 +1,35 @@
 // TypeScript's Interface object type keyword, which is one way to define the "shape" an object should have
-export interface CourseParts {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
+  type: string;
 }
+
+interface CoursePartWithDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CourseNormalPart extends CoursePartWithDescription {
+  type: 'normal';
+}
+
+interface CourseProjectPart extends CoursePartBase {
+  type: 'groupProject';
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartWithDescription {
+  type: 'submission';
+  exerciseSubmissionLink: string;
+}
+
+interface CourseRequirementsPart extends CoursePartWithDescription {
+  type: 'special';
+  requirements: string[];
+}
+
+export type CoursePart =
+  | CourseNormalPart
+  | CourseProjectPart
+  | CourseSubmissionPart
+  | CourseRequirementsPart;
