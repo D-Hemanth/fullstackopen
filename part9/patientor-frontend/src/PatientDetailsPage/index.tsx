bbb71dded4b265b18b13645c83ca4da043407f67
Hemanth-D <hemanthh531@gmail.com>
2022-09-30 11:43:49 +0530

    part9 9.18: Complete patientor step3

--- part9/patientor-frontend/src/PatientDetailsPage/index.tsx
+++ part9/patientor-frontend/src/PatientDetailsPage/index.tsx
@@ -1,4 +1,13 @@
+import React from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useStateValue } from "../state";
+import { Patient } from "../types";
+import { apiBaseUrl } from "../constants";
 import { setPatientList } from "../state";
+import MaleIcon from "@mui/icons-material/Male";
+import FemaleIcon from "@mui/icons-material/Female";
+import TransgenderIcon from "@mui/icons-material/Transgender";
 
 const PatientDetailsPage = () => {
   const [{ confidentialPatientInfo }, dispatch] = useStateValue();
@@ -20,13 +29,13 @@ const PatientDetailsPage = () => {
         setPatient(patient);
       } catch (e: unknown) {
         if (axios.isAxiosError(e)) {
-          console.error(e?.response?.data || 'Unrecognized axios error');
+          console.error(e?.response?.data || "Unrecognized axios error");
           setError(
-            String(e?.response?.data?.error) || 'Unrecognized axios error'
+            String(e?.response?.data?.error) || "Unrecognized axios error"
           );
         } else {
-          console.error('Unknown error', e);
-          setError('Unknown error');
+          console.error("Unknown error", e);
+          setError("Unknown error");
         }
       }
     };
@@ -46,10 +55,10 @@ const PatientDetailsPage = () => {
 
   if (patient) {
     switch (patient.gender) {
-      case 'male':
+      case "male":
         iconName = <MaleIcon />;
         break;
-      case 'female':
+      case "female":
         iconName = <FemaleIcon />;
         break;
       default:
@@ -62,7 +71,7 @@ const PatientDetailsPage = () => {
   return (
     <div>
       {error && (
-        <div style={{ padding: '10px', border: '2px solid red' }}>{error}</div>
+        <div style={{ padding: "10px", border: "2px solid red" }}>{error}</div>
       )}
       <h2>
         {patient.name} {iconName}
