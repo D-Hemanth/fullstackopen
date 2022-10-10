4b2e46c44b6504845ed47ee7eb0ee32574734362
Hemanth-D <hemanthh531@gmail.com>
2022-10-10 16:44:43 +0530

    part9 9.19: Modify Entry type to be union of HealthCheck, OccupationHealthcare, Hospital interface with Base Entry

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -14,8 +14,50 @@ export enum Gender {
   Other = "other",
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
+// When looking at the type, we can see that there are actually three kinds of entries:
+// OccupationalHealthcare, Hospital and HealthCheck. This indicates we need three separate types.
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<DiagnosesEntry["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: String;
+  sickLeave?: {
+    startDate: String;
+    endDate: String;
+  };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: String;
+    criteria: String;
+  };
+}
+
+// since we need three kinds of entries: OccupationalHealthcareEntry, HospitalEntry and HealthCheckEntry types so we can combine them in a union
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface PatientsEntry {
   id: string;
--- part9/patientor-frontend/src/types.ts
+++ part9/patientor-frontend/src/types.ts
@@ -7,8 +7,54 @@ export interface Diagnosis {
 export enum Gender {
   Male = "male",
   Female = "female",
-  Other = "other"
+  Other = "other",
 }
+
+// When looking at the type, we can see that there are actually three kinds of entries:
+// OccupationalHealthcare, Hospital and HealthCheck. This indicates we need three separate types.
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: String;
+  sickLeave?: {
+    startDate: String;
+    endDate: String;
+  };
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: String;
+    criteria: String;
+  };
+}
+
+// since we need three kinds of entries: OccupationalHealthcareEntry, HospitalEntry and HealthCheckEntry types so we can combine them in a union
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface Patient {
   id: string;
@@ -17,4 +63,5 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 }
