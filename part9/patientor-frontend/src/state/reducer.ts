11d3d29cd2510b2e8b26c70494ebadfbf2e1ad69
Hemanth-D <hemanthh531@gmail.com>
2022-10-10 21:26:38 +0530

    part9 9.21: Create a diagnosisList reducer & setDiagnosislist action creator

--- part9/patientor-frontend/src/state/reducer.ts
+++ part9/patientor-frontend/src/state/reducer.ts
@@ -1,23 +1,27 @@
-import { State } from "./state";
-import { Patient } from "../types";
+import { State } from './state';
+import { Diagnosis, Patient } from '../types';
 
 export type Action =
   | {
-      type: "SET_PATIENT_LIST";
+      type: 'SET_PATIENT_LIST';
       payload: Patient[];
     }
   | {
-      type: "ADD_PATIENT";
+      type: 'ADD_PATIENT';
       payload: Patient;
     }
   | {
-      type: "FETCHED_PATIENT";
+      type: 'FETCHED_PATIENT';
       payload: Patient;
+    }
+  | {
+      type: 'SET_DIAGNOSIS_LIST';
+      payload: Diagnosis[];
     };
 
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
-    case "SET_PATIENT_LIST":
+    case 'SET_PATIENT_LIST':
       return {
         ...state,
         patients: {
@@ -28,7 +32,7 @@ export const reducer = (state: State, action: Action): State => {
           ...state.patients,
         },
       };
-    case "ADD_PATIENT":
+    case 'ADD_PATIENT':
       return {
         ...state,
         patients: {
@@ -36,12 +40,22 @@ export const reducer = (state: State, action: Action): State => {
           [action.payload.id]: action.payload,
         },
       };
-    case "FETCHED_PATIENT":
+    case 'FETCHED_PATIENT':
       return {
         ...state,
         confidentialPatientInfo: {
           ...state.confidentialPatientInfo,
           [action.payload.id]: action.payload,
+        },
+      };
+    case 'SET_DIAGNOSIS_LIST':
+      return {
+        ...state,
+        diagnosisList: {
+          ...action.payload.reduce(
+            (memo, diagnosis) => ({ [diagnosis.code]: diagnosis, ...memo }),
+            {}
+          ),
         },
       };
     default:
@@ -51,7 +65,11 @@ export const reducer = (state: State, action: Action): State => {
 
 export const setPatientList = (patient: Patient): Action => {
   return {
-    type: "FETCHED_PATIENT",
+    type: 'FETCHED_PATIENT',
     payload: patient,
   };
+};
+
+export const setDiagnosisList = (diagnosisCodes: Diagnosis[]): Action => {
+  return { type: 'SET_DIAGNOSIS_LIST', payload: diagnosisCodes };
 };
